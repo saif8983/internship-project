5a60ef8ccade276a53435c0ea49b901994013303
saif8983 <asaif8367@gmail.com>
2022-08-18 01:32:03 +0530

    Navebar completed

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,28 +1,31 @@
 import React from "react";
 import "../App.css"
-import { IconsSrc } from "../Constant";
-// Navebar component 
+import IconsSrc from "../utils/icons";
+//navbar components
 const Navbar = () => {
 
-	return (
-		<>
-			<div class="main-navbar">
-				<div class="left-navbar">
-					<div className="left-navbar-image"  ></div>
-					<div className="left-title-navbar"><h3>Dezzie</h3></div>
-				</div>
-				<div class="right-navbar">
-					<img src={IconsSrc.Bell} alt="no icon"></img>
-					<div className="right-navbar-image"></div>
-					<div>
-						<div className="right-title-navbar">Sumanto</div>
-						<div className="navbar-cashier">Cashier</div>
-					</div>
-				</div>
-			</div>
-		</>
+  return (
+    //structure of navbar which alingment done by flex
+    <>
+      <div class="main-navbar">
+        {/*left navebar section which gap and alignment main by flex */}
+        <div class="left-navbar">
+          <div className="left-navbar-image"  ></div>
+          <div className="left-title-navbar"><h3>Dezzie</h3></div>
+        </div>
+        {/*right navebar section which gap and alignment main by flex*/}
+        <div class="right-navbar">
+          <img src={IconsSrc.Bell} alt="no icon"></img>
+          <div className="right-navbar-image"></div>
+          <div>
+            <div className="right-title-navbar">Sumanto</div>
+            <div className="navbar-cashier">Cashier</div>
+          </div>
+        </div>
+      </div>
+    </>
 
-	);
+  );
 
 }
 
