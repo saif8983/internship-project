30178bc0bd19e3d0f78ad1897d1d0aae1130a45f
saif8983 <asaif8367@gmail.com>
2022-08-18 01:23:20 +0530

    modified sidebar

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,27 +1,28 @@
 import React from "react";
 import "../App.css"
 import { IconsSrc } from "../Constant";
+// Navebar component 
 const Navbar = () => {
 
-    return (
-        <>
-            <div class="main-navbar">
-                <div class="left-navbar">
-                    <div className="left-navbar-image"  ></div>
-                    <div className="left-title-navbar"><h3>Dezzie</h3></div>
-                </div>
-                <div class="right-navbar">
-                    <img src={IconsSrc.Bell} alt="no icon"></img>
-                    <div className="right-navbar-image"></div>
-                    <div>
-                        <div className="right-title-navbar">Sumanto</div>
-                        <div className="navbar-cashier">Cashier</div>
-                    </div>
-                </div>
-            </div>
-        </>
+	return (
+		<>
+			<div class="main-navbar">
+				<div class="left-navbar">
+					<div className="left-navbar-image"  ></div>
+					<div className="left-title-navbar"><h3>Dezzie</h3></div>
+				</div>
+				<div class="right-navbar">
+					<img src={IconsSrc.Bell} alt="no icon"></img>
+					<div className="right-navbar-image"></div>
+					<div>
+						<div className="right-title-navbar">Sumanto</div>
+						<div className="navbar-cashier">Cashier</div>
+					</div>
+				</div>
+			</div>
+		</>
 
-    );
+	);
 
 }
 
--- src/Components/Sidebar.jsx
+++ src/Components/Sidebar.jsx
@@ -1,14 +1,14 @@
 import "../App.css"
 import { useState } from "react"
 import { useNavigate } from "react-router-dom"
-// All icons are imported by constant file which variable have array of object
-import { IconsSrc } from "../Constant"
-//sidebar component
+import IconsSrc from "../utils/icons"
+//sidebar component 
 const Sidebar = () => {
-  //usestate used for change icon background change in inline css
-  const [homebackGroundColor,setHomeBackGroundColor]=useState('')
-  const [customerBackGroundColor,setCustomerBackGroundColor]=useState('')
+  //state variable to give background color to icons daynamically in inline css
+  const [homebackGroundColor, setHomeBackGroundColor] = useState('')
+  const [customerBackGroundColor, setCustomerBackGroundColor] = useState('')
   const navigate = useNavigate()
+  //handlehome and handleCustomer function which will invoked press grid icon and usericons 
   const handleHome = () => {
     navigate("/")
     setHomeBackGroundColor('#5541d7')
@@ -25,14 +25,12 @@ const Sidebar = () => {
       <div className="main-sidebar" >
         <div>
           <ul className="sidebar">
-            <li style={{backgroundColor:homebackGroundColor}} ><span><i className="fa fa-home" ></i></span><span ><img onClick={() => handleHome()} src={IconsSrc.Home} alt="no icon" ></img></span></li>
-            <li><span><i className="fa fa-order"></i></span><span><img src={IconsSrc.Oders} alt="no icon"></img></span></li>
-            <li style={{backgroundColor:customerBackGroundColor}}><span><i className="fa fa-users"></i></span><span><img onClick={() => handleCustomer()} src={IconsSrc.Customer} alt="no icon"></img></span></li>
-            <li><span><i className="fa fa-shopping-cart"></i></span><span><img src={IconsSrc.Package} alt="no icon"></img></span></li>
-            <li><span><i className="fa fa-bookmark"></i></span><span><img src={IconsSrc.Coupon} alt="no icon"></img></span></li>
-            <li><span><i className="fa fa-gear"></i></span><span><img src={IconsSrc.User} alt="no icon"></img></span></li>
-
-
+            <li style={{ backgroundColor: homebackGroundColor }} ><span><i className="fa fa-home" ></i></span><span ><img onClick={() => handleHome()} src={IconsSrc.Home} alt="no icon" ></img></span></li>
+            <li><span><i ></i></span><span><img src={IconsSrc.Oders} alt="no icon"></img></span></li>
+            <li style={{ backgroundColor: customerBackGroundColor }}><span><i className="fa fa-users"></i></span><span><img onClick={() => handleCustomer()} src={IconsSrc.Customer} alt="no icon"></img></span></li>
+            <li><span><i></i></span><span><img src={IconsSrc.Package} alt="no icon"></img></span></li>
+            <li><span><i></i></span><span><img src={IconsSrc.Coupon} alt="no icon"></img></span></li>
+            <li><span><i></i></span><span><img src={IconsSrc.User} alt="no icon"></img></span></li>
             <div className="setting"><img src={IconsSrc.Setting} alt="no icon"></img></div>
           </ul>
         </div>
