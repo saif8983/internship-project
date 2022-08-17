6f096e739f125e03c47e45b01eb693292a6baf1b
saif8983 <asaif8367@gmail.com>
2022-08-18 01:55:24 +0530

    home page

---
+++ src/pages/Customer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import Customerpage from '../Components/Customerpage';
+const Customer = () => {
+    return (
+        <>
+          <Customerpage/>  
+        </>
+    );
+}
+
+export default Customer;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Homapage from "../Components/Homapage";
+// homepage is import to pages folder component
+const Home = () => {
+    return (
+        <>
+        <Homapage/>    
+        </>
+    );
+}
+
+export default Home;
